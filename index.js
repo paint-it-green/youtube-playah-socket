9857c0eb3c7579a579447ad79854b27b333c8a2c
Gerwin <gerwin@hyperstacksinc.com>
2018-12-14 12:18:28 +0800

    config

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+  transports  : [ 'websocket' ],
+  pollingDuration : 10
+});
+
+var port = 5000;
+
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
@@ -56,6 +62,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:5000');
 });
